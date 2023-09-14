175d5642f9a9bc31b0302be1d90d7450cf44d335
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-09-14 12:42:30 -0300

    :label: feat: .env

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { App } from "./app";
+import { App } from "./app.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 
 import "./index.css";
